df7458ae3f73738140ecd43c031f8b6cd2fa8bd3
shijunlin-miaozi <74685676+shijunlin-miaozi@users.noreply.github.com>
2021-01-07 15:07:15 +0100

    update

--- P2_sudoku_solver.py
+++ P2_sudoku_solver.py
@@ -9,7 +9,19 @@ s= [[4, 0, 0,   0, 0, 0,   0, 7, 8],
 
     [0, 0, 0,   5, 0, 0,   0, 0, 0],
     [8, 1, 3,   0, 0, 0,   0, 0, 7],
-    [0, 0, 0,   0, 2, 0,   0, 0, 0]] # solved with 22 guess
+    [0, 0, 0,   0, 2, 0,   0, 0, 0]] # solved with 12 guess
+
+# s= [[0, 0, 0,   4, 1, 0,   0, 0, 0], 
+#     [0, 8, 0,   7, 0, 0,   2, 0, 0],
+#     [0, 7, 0,   0, 0, 0,   0, 5, 0],
+
+#     [0, 0, 6,   0, 0, 0,   3, 0, 0],
+#     [2, 0, 0,   0, 9, 6,   0, 7, 0],
+#     [0, 0, 0,   0, 8, 4,   0, 0, 0],
+
+#     [5, 3, 0,   0, 0, 0,   8, 0, 4],
+#     [0, 0, 0,   0, 0, 9,   0, 6, 0],
+#     [0, 4, 0,   0, 0, 1,   0, 0, 0]] # solved with 9 guess
 
 from itertools import combinations
 import copy
@@ -80,18 +92,31 @@ def make_cstr_dict(d):
             cstr_d[i] = cstr_d.setdefault(i, set()) | {label}
     return cstr_d
     
-def sole_poss(d):
+def eliminate(to_remove, remove_from, d): # 1st para is cstr set to be eliminated, 2nd is label set from which cstr is eliminated
     ctr = 0
+    for el in to_remove:
+        for label in remove_from:
+            if el in d['blank'][label]:
+                d['blank'][label].remove(el)
+                ctr = 1
+    return ctr, d
+
+def union_n_diff(d, label_set1, label_set2=set()): # 2nd & 2rd para are label sets, return diff in cstr union of the 2 label sets
+    cstr1, cstr2 = set(), set()
+    for x, y in zip((label_set1, label_set2), (cstr1, cstr2)):
+        for label in x:
+            y |= d['blank'][label]
+    return cstr1 - cstr2
+
+def sole_poss(d): # confirm a blank when it contains a cstr num that does not appear in other blanks in a row, column or box
     cstr_d = make_cstr_dict(d)
     for j in cstr_d:
         for label in cstr_d[j]:
             n = union_n_diff(d, cstr_d[j], cstr_d[j] - {label})
             if len(n) == 1 and len(d['blank'][label]) != 1:
-                ctr += 1
                 d['blank'][label] = n
                 print('....................sole_poss')
-    print('total sole_poss: ', ctr, 'times')
-    return d
+    return d 
 
 def direct_solve(d, ref):
     ctr = 0
@@ -103,23 +128,7 @@ def direct_solve(d, ref):
     print('----------------direct solve iteration: ', ctr, '     total:', len(d['blank']) + len(d['ans']), '      solved:', len(d['ans']), '      UNSOLVED:', len(d['blank']))
     return d
 
-def eliminate(to_remove, remove_from, d): # 1st para is cstr set to be eliminated, 2nd is label set from which cstr is eliminated, 3rd is sudoku dict
-    ctr = 0
-    for el in to_remove:
-        for label in remove_from:
-            if el in d['blank'][label]:
-                d['blank'][label].remove(el)
-                ctr = 1
-    return ctr, d
-
-def union_n_diff(d, label_set1, label_set2=set()): # 1st para is sudoku dict, 2nd & 2rd para are label sets, return diff in cstr union of the 2 label sets
-    cstr1, cstr2 = set(), set()
-    for x, y in zip((label_set1, label_set2), (cstr1, cstr2)):
-        for label in x:
-            y |= d['blank'][label]
-    return cstr1 - cstr2
-
-def indirect_solve(d, ref):
+def indirect_solve(d, ref): # overview of all rules http://www.pennydellsudokusolver.com/help/tips.html
     cstr_d = make_cstr_dict(d)
     ctr = 1
     while ctr > 0:
@@ -176,86 +185,82 @@ def indirect_solve(d, ref):
                             return d
         if has_update(sole_poss(d)): 
             return d
-        pair_d = {}
-        for i in range(1, 10):
-            pair_d.setdefault(i, {'r': {}, 'c': {}})
-        for j in cstr_d:
-            if j[0] == 'b': continue
-            for a, b in list(combinations(cstr_d[j], 2)):
-                n = union_n_diff(d, cstr_d[j], cstr_d[j] - {a, b})
-                if len(n) == 1 and n.issubset(d['blank'][a]) and n.issubset(d['blank'][b]):
-                    [a, b], r_d, c_d = sorted([a, b]), pair_d[list(n)[0]]['r'], pair_d[list(n)[0]]['c']
-                    if a[0] == b[0]: 
-                        r_d[(a[1][1], b[1][1])] = r_d.setdefault((a[1][1], b[1][1]), []) + [(a, b)]
-                    else: 
-                        c_d[(a[0][1], b[0][1])] = c_d.setdefault((a[0][1], b[0][1]), []) + [(a, b)]
-        for num, val1 in pair_d.items():
-            for r_c, val2 in val1.items():
-                if len(val2) > 2:
-                    for a, b, c in list(combinations(val2.keys(), 3)):
-                        uni = list(set(a) | set(b) | set(c))
-                        if len(uni) == 3: # deduction - swordfish
-                            line = list({'r', 'c'} - set(r_c))[0]
-                            all_blank, multi_pair, k = set(), set(), (a, b, c)
-                            for i, k in zip((0, 1, 2), (a, b, c)):
-                                all_blank |= cstr_d[line + uni[i]]
-                                multi_pair |= {val2[k][0][0], val2[k][0][1]}
-                            n, d = eliminate(set([num]), all_blank - multi_pair, d)
-                            if n == 1: print('........................has swordfish')
-                            ctr += n
-                            if has_update(d): 
-                                return d
-                for pos, pair in val2.items():
-                    if len(pair) == 2: # deduction - x-wing
-                        line = list({'r', 'c'} - set(r_c))[0]
-                        remove_fr = (cstr_d[line + pos[0]] | cstr_d[line + pos[1]]) - (set(pair[0]) | set(pair[1]))
-                        n, d = eliminate(set([num]), remove_fr, d)
-                        if n == 1: print('....................has x-wing')
-                        ctr += n
-                        if has_update(d): 
-                            return d
-        label_set = set(label for label, cstr in d['blank'].items() if len(cstr) == 2) 
-        for x, y, z in list(combinations(label_set, 3)): 
-            if x[0] == y[0] == z[0] or x[1] == y[1] == z[1] or x[2] == y[2] == z[2]: continue
-            if len(union_n_diff(d, {x, y, z})) == 3 and d['blank'][x] != d['blank'][y] != d['blank'][z] != d['blank'][x]:
-                seen, lt = set(), [k[i] for k in [x, y, z] for i in range(3)]
-                dups = set(x for x in lt if x in seen or seen.add(x))
-                if len(dups) > 1: # deduction - xy-wing
-                    if 'b' not in str(dups):
-                        A = [k for k in [x, y, z] if dups.issubset(set(k))][0]
-                        (B, C) = tuple({x, y, z} - {A})
-                        label1, label2 = ref[int(C[0][1]) - 1, int(B[1][1]) - 1], ref[int(B[0][1]) - 1, int(C[1][1]) - 1]
-                        remove_fr = [x for x in [label1, label2] if x in d['blank'] and x != A]
-                        n, d = eliminate(union_n_diff(d, {B}, {A}), remove_fr, d)
-                        if n == 1: print('............................has xy-wing (in 3 boxes)')
-                        ctr += n
-                        if has_update(d): 
-                            return d
-                    else:
-                        b_pair = 'b' + str(dups)[str(dups).index('b') + 1]
-                        C = [k for k in [x, y, z] if k[2] != b_pair][0]
-                        b_other, r_c = C[2], [x for x in dups if x in C]
-                        if len(r_c) == 1:
-                            A = [k for k in ({x, y, z} - {C}) if r_c[0] in k][0]
-                            B, r_c_pair = list({x, y, z} - {A, C})[0], r_c[0]
-                            r_c_other = r_c_pair[0] + str(B)[str(B).index(r_c_pair[0]) + 1]
-                            remove_fr = ((cstr_d[b_pair] & cstr_d[r_c_pair]) - {A}) | (cstr_d[b_other] & cstr_d[r_c_other])
-                            n, d = eliminate(union_n_diff(d, {B}, {A}), remove_fr, d)
-                            if n == 1: print('............................has xy-wing (in 2 boxes)')
-                            ctr += n
-                            if has_update(d): 
-                                return d
+        # pair_d = {}
+        # for i in range(1, 10):
+        #     pair_d.setdefault(i, {'r': {}, 'c': {}})
+        # for j in cstr_d:
+        #     if j[0] == 'b': continue
+        #     for a, b in list(combinations(cstr_d[j], 2)):
+        #         n = union_n_diff(d, cstr_d[j], cstr_d[j] - {a, b})
+        #         if len(n) == 1 and n.issubset(d['blank'][a]) and n.issubset(d['blank'][b]):
+        #             [a, b], r_d, c_d = sorted([a, b]), pair_d[list(n)[0]]['r'], pair_d[list(n)[0]]['c']
+        #             if a[0] == b[0]: 
+        #                 r_d[(a[1][1], b[1][1])] = r_d.setdefault((a[1][1], b[1][1]), []) + [(a, b)]
+        #             else: 
+        #                 c_d[(a[0][1], b[0][1])] = c_d.setdefault((a[0][1], b[0][1]), []) + [(a, b)]
+        # for num, val1 in pair_d.items():
+        #     for r_c, val2 in val1.items():
+        #         if len(val2) > 2:
+        #             for a, b, c in list(combinations(val2.keys(), 3)):
+        #                 uni = list(set(a) | set(b) | set(c))
+        #                 if len(uni) == 3: # deduction - swordfish
+        #                     line, all_blank, multi_pair = list({'r', 'c'} - set(r_c))[0], set(), set()
+        #                     for i, k in zip((0, 1, 2), (a, b, c)):
+        #                         all_blank |= cstr_d[line + uni[i]]
+        #                         multi_pair |= {val2[k][0][0], val2[k][0][1]}
+        #                     n, d = eliminate(set([num]), all_blank - multi_pair, d)
+        #                     if n == 1: print('........................has swordfish')
+        #                     ctr += n
+        #                     if has_update(d): 
+        #                         return d
+        #         for pos, pair in val2.items():
+        #             if len(pair) == 2: # deduction - x-wing
+        #                 line = list({'r', 'c'} - set(r_c))[0]
+        #                 remove_fr = (cstr_d[line + pos[0]] | cstr_d[line + pos[1]]) - (set(pair[0]) | set(pair[1]))
+        #                 n, d = eliminate(set([num]), remove_fr, d)
+        #                 if n == 1: print('....................has x-wing')
+        #                 ctr += n
+        #                 if has_update(d): 
+        #                     return d
+        # label_set = set(label for label, cstr in d['blank'].items() if len(cstr) == 2) 
+        # for x, y, z in list(combinations(label_set, 3)): 
+        #     if x[0] == y[0] == z[0] or x[1] == y[1] == z[1] or x[2] == y[2] == z[2]: continue
+        #     if len(union_n_diff(d, {x, y, z})) == 3 and d['blank'][x] != d['blank'][y] != d['blank'][z] != d['blank'][x]:
+        #         seen, lt = set(), [k[i] for k in [x, y, z] for i in range(3)]
+        #         dups = set(x for x in lt if x in seen or seen.add(x))
+        #         if len(dups) > 1: # deduction - xy-wing
+        #             if 'b' not in str(dups):
+        #                 A = [k for k in [x, y, z] if dups.issubset(set(k))][0]
+        #                 (B, C) = tuple({x, y, z} - {A})
+        #                 label1, label2 = ref[int(C[0][1]) - 1, int(B[1][1]) - 1], ref[int(B[0][1]) - 1, int(C[1][1]) - 1]
+        #                 remove_fr = [x for x in [label1, label2] if x in d['blank'] and x != A]
+        #                 n, d = eliminate(union_n_diff(d, {B}, {A}), remove_fr, d)
+        #                 if n == 1: print('............................has xy-wing (in 3 boxes)')
+        #                 ctr += n
+        #                 if has_update(d): 
+        #                     return d
+        #             else:
+        #                 b_pair = 'b' + str(dups)[str(dups).index('b') + 1]
+        #                 C = [k for k in [x, y, z] if k[2] != b_pair][0]
+        #                 b_other, r_c = C[2], [x for x in dups if x in C]
+        #                 if len(r_c) == 1:
+        #                     A = [k for k in ({x, y, z} - {C}) if r_c[0] in k][0]
+        #                     B, r_c_pair = list({x, y, z} - {A, C})[0], r_c[0]
+        #                     r_c_other = r_c_pair[0] + str(B)[str(B).index(r_c_pair[0]) + 1]
+        #                     remove_fr = ((cstr_d[b_pair] & cstr_d[r_c_pair]) - {A}) | (cstr_d[b_other] & cstr_d[r_c_other])
+        #                     n, d = eliminate(union_n_diff(d, {B}, {A}), remove_fr, d)
+        #                     if n == 1: print('............................has xy-wing (in 2 boxes)')
+        #                     ctr += n
+        #                     if has_update(d): 
+        #                         return d
     return None, d
-
-# * change done: all rules done, all rules confirmed except xy-wing(in 3 boxes); added guess code
-# TODO: run test on last sample to check whether solver can solve all; modify guess code to guess 2 cells after exhausted first cell (in 1 eg it takes 22x guess to solve, if guess 2 cells it will be only 4x, 3 cells 8x)
 
 def rule_only_solve(d, ref):
     ctr = 0
     while not (d['blank'] == {} and final_check(d)):
         d = indirect_solve(d, ref['row_index_to_label'])
         if isinstance(d, tuple):
-            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> deduction exhausted >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> rule only solve exhaused, go to guess >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
             return d[1]
         d = direct_solve(d, ref['label_to_index_map'])
         ctr += 1
@@ -271,21 +276,40 @@ def sudoku_solver(s, ref):
         return d
     lt = sorted([[len(cstr), list(cstr), label] for label, cstr in d['blank'].items()])
     ctr = 0
-    for el in lt:
+    for el in lt: # guess one blank at one time
         for cstr in el[1]:
             d1 = copy.deepcopy(d)
             d1['blank'][el[2]] = {cstr}
+            print('label: ', el[2], '   cstr guess: ', {cstr})
             ctr += 1
             d1 = rule_only_solve(d1, ref)
             if isinstance(d1, list):
                 print('guess iteration is: ', ctr)
                 return d1
             else: continue
-    
+    i, guess = 0, [[]] # guess multiple blanks at same time
+    while i < len(lt):
+        guess = [y + [x] for x in lt[i][1] for y in guess]
+        for combi in guess:
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> combi is: ', combi)
+            d1 = copy.deepcopy(d)
+            ctr += 1
+            print('ctr is: ', ctr)
+            for index, cstr in zip(range(i + 1), combi):
+                d1['blank'][lt[index][2]] = {cstr}
+                d1 = rule_only_solve(d1, ref)
+                if isinstance(d1, list):
+                    print('guess iteration is: ', ctr)
+                    return d1
+                else: continue
+        i += 1
+
+# * change done: added guess code
+# ? guess one blank at one time or multiple ones at one time? 
+# ? remove complex rules (swordfish, x-wing, xy-wing) to improve efficiency? heavy code but rarely used, time wasted in checking
+# TODO: run test on large sample
 
 if __name__ == '__main__':
-    pass
-
     import time
     start_time = time.time()
     ref = ref_table()
