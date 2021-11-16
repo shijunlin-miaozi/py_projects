bc81b73af161d2a65750c5966b8a29ba9298564e
shijunlin <miaozilsj@gmail.com>
2021-11-16 14:43:55 +0100

    update

--- P5_covid_data.py
+++ P5_covid_data.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
-import numpy as np
 import json
 from urllib.request import urlopen
+from datetime import datetime, timedelta, date
 
 # source data
 json_hosp = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/hospitalizace.json"
 json_infect = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.json"
 path_vac = '/Users/shijunlin/Desktop/current/ockovani-pozitivity-hospitalizace-jip.xlsx' 
+# download vaccine data here: https://data.nzis.cz/news-detail/cs/48-covid-19-nove-otevrene-datove-sady-s-ohledem-na-vykazana-ockovani/
 
 
 def get_data(url):
@@ -30,12 +31,52 @@ df_hosp_ratio = (df_hosp / df_infect)
 df_severe_ratio = (df_severe / df_hosp)
 
 df_vac = pd.read_excel(path_vac, sheet_name = None)
-df_poz, df_hos, df_jip = df_vac['pozitivity'].groupby('od').sum(), df_vac['hospitalizace'].groupby('od').sum(), df_vac['jip'].groupby('od').sum()
-for df in (df_poz, df_hos, df_jip):
-    df.sort_values(by=['od'], inplace=True)
+df_poz, df_hos, df_jip = df_vac['pozitivity'], df_vac['hospitalizace'], df_vac['jip']
 
-pd.set_option('display.max_rows', None)
-print(df_jip)
+cutoff = 2021, 9, 1
+df_poz = df_poz[(df_poz['od'].dt.date >= date(*cutoff))]
+df_hos = df_hos[(df_hos['od'].dt.date >= date(*cutoff))]
+df_jip = df_jip[(df_jip['od'].dt.date >= date(*cutoff))]
+
+start_d, end_d = df_poz['od'].min(), df_poz['od'].max() + timedelta(days=6)
+
+df_poz = df_poz.assign(age = df_poz['vek_kategorie'].apply(lambda x: x.split('-')[0] if len(x.split('-')[0]) < 2 else x.split('-')[0][:2]).values)
+df_hos = df_hos.assign(age = df_hos['vek_kategorie'].apply(lambda x: x.split('-')[0] if len(x.split('-')[0]) < 2 else x.split('-')[0][:2]).values)
+df_jip = df_jip.assign(age = df_jip['vek_kategorie'].apply(lambda x: x.split('-')[0] if len(x.split('-')[0]) < 2 else x.split('-')[0][:2]).values)
+
+df_poz = df_poz.assign(age_gp = df_poz['age'].apply(lambda x: 'unknown' if x == 'ne' else '0-24' if int(x) < 25 else '65+' if int(x) > 64 else '25-64').values)
+df_hos = df_hos.assign(age_gp = df_hos['age'].apply(lambda x: 'unknown' if x == 'ne' else '0-24' if int(x) < 25 else '65+' if int(x) > 64 else '25-64').values)
+df_jip = df_jip.assign(age_gp = df_jip['age'].apply(lambda x: 'unknown' if x == 'ne' else '0-24' if int(x) < 25 else '65+' if int(x) > 64 else '25-64').values)
+
+df_poz_bydate, df_hos_bydate, df_jip_bydate = df_poz.groupby('od').sum().sum(), df_hos.groupby('od').sum().sum(), df_jip.groupby('od').sum().sum()
+df_poz_byage, df_hos_byage, df_jip_byage = df_poz.groupby('age_gp').sum(), df_hos.groupby('age_gp').sum(), df_jip.groupby('age_gp').sum()
+
+df_poz_byage = df_poz_byage.assign(w_vac = (df_poz_byage.iloc[:, 0] - df_poz_byage.iloc[:, 1]).values)
+df_hos_byage = df_hos_byage.assign(w_vac = (df_hos_byage.iloc[:, 0] - df_hos_byage.iloc[:, 1]).values)
+df_jip_byage = df_jip_byage.assign(w_vac = (df_jip_byage.iloc[:, 0] - df_jip_byage.iloc[:, 1]).values)
+
+
+label_vac_pie = ['Without Vac', 'With 1 Shot', 'With 2 Shots', 'With Booster']
+
+value_poz_pie = df_poz_bydate.iloc[1], df_poz_bydate.iloc[2], df_poz_bydate.iloc[3], df_poz_bydate.iloc[4]
+value_hos_pie = df_hos_bydate.iloc[1], df_hos_bydate.iloc[2], df_hos_bydate.iloc[3], df_hos_bydate.iloc[4]
+value_jip_pie = df_jip_bydate.iloc[1], df_jip_bydate.iloc[2], df_jip_bydate.iloc[3], df_jip_bydate.iloc[4]
+
+
+label_vac_bar = ['Total', 'Without Vac', 'With Vac']
+
+value_poz_bar = df_poz_byage.iloc[:, [0, 1, -1]].transpose()
+value_hos_bar = df_hos_byage.iloc[:, [0, 1, -1]].transpose()
+value_jip_bar = df_jip_byage.iloc[:, [0, 1, -1]].transpose()
+
+for df in (value_poz_bar, value_hos_bar, value_jip_bar):
+    df.index = label_vac_bar
+
+# pd.set_option('display.max_rows', None)
+# print(start_d, end_d)
+# print(value_poz_bar)
+# print(value_hos_bar)
+# print(value_jip_bar)
 
 
 x = df_date_infect
@@ -80,7 +121,6 @@ ylim_infect_max = (max(y1_2020.max(), y1_2021.max()) * -1 // 500) * -500
 ylim_ratio_max = (max(y6_2020.max(), y6_2021.max()) * -1 // 0.05) * -0.05
 
 
-
 def plot_infect_hosp_severe_whole():
     fig, ax = plt.subplots(nrows=2, ncols=1, figsize = (14, 7))
 
@@ -99,7 +139,7 @@ def plot_infect_hosp_severe_whole():
     ax[0].set(title=f'Covid Situation in Czech Republic from {x_min: %Y-%b-%d} to {x_max: %Y-%b-%d})', ylabel='no. of patients')
     ax[1].set(ylabel='ratio')
     fig.autofmt_xdate()
-    plt.show()
+    return fig
 
 
 def plot_infect_hosp_severe_parts():
@@ -129,9 +169,37 @@ def plot_infect_hosp_severe_parts():
     ax[2].set(xlim=[x_2020_min, x_2020_max], ylim=[0,ylim_ratio_max], ylabel='ratio')
     ax[3].set(xlim=[x_2021_min, x_2021_max], ylim=[0,ylim_ratio_max], ylabel='ratio')
     fig.autofmt_xdate()
-    plt.show()
+    return fig
+
+
+def plot_vac():
+    fig, ax = plt.subplots(nrows=2, ncols=3, figsize = (14, 7))
+    
+    ax = ax.flatten()
+    ax[0].pie(value_poz_pie, labels = label_vac_pie, autopct = '%1.0f%%')
+    ax[1].pie(value_hos_pie, labels = label_vac_pie, autopct = '%1.0f%%')
+    ax[2].pie(value_jip_pie, labels = label_vac_pie, autopct = '%1.0f%%')
+
+    ax[0].set_title('For Infected Patients')
+    ax[1].set_title('For Hospitalised Patients')
+    ax[2].set_title('For ICU Patients')
+
+    value_poz_bar.iloc[:, [2, 1, 0]].plot.bar(ax = ax[3], stacked = True, rot = 0, color ='rcm')
+    value_hos_bar.iloc[:, [2, 1, 0]].plot.bar(ax = ax[4], stacked = True, rot = 0, color ='rcm')
+    value_jip_bar.iloc[:, [2, 1, 0]].plot.bar(ax = ax[5], stacked = True, rot = 0, color ='rcm')
+
+    for i in ax[3:]:
+        for c in i.containers:
+            i.bar_label(c, label_type='center', fmt = '%g')
+
+    fig.suptitle(f'Covid Patients Vaccination Profile Breakdown\n(aggregated value from {start_d:%Y-%b-%d} to {end_d:%Y-%b-%d})')
+    return fig
+
 
 if __name__ == '__main__':
-    pass
-    # plot_infect_hosp_severe_whole()
-    # plot_infect_hosp_severe_parts()
+
+    fig1 = plot_infect_hosp_severe_whole()
+    fig2 = plot_infect_hosp_severe_parts()
+    fig3 = plot_vac()
+    plt.show()
+
